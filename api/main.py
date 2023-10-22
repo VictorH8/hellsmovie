763575568245939d5f0b4d63da3a1281cc0548c8
Victor <hugovictor.dev@gmail.com>
2023-10-22 20:25:09 -0300

    X

--- api/main.py
+++ api/main.py
@@ -4,11 +4,12 @@ import json
 
 
 app = Flask(__name__)
+TMDB_API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMmM2YWNkNDM4OTVjZGVlMzkwNWRhODhjZDdjOTNjZiIsInN1YiI6IjY1MGYxYWQ2MjZkYWMxMDEyZDVhOTRjYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.P2xiPnZCI13rJqk-Pe7KWfIgVRbYf80777VvzGoNdik'
 
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=pt-BR&page=90&sort_by=popularity.desc"
+    url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=pt-BR&page=430&sort_by=popularity.desc"
     headers = {"accept": "application/json","Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMmM2YWNkNDM4OTVjZGVlMzkwNWRhODhjZDdjOTNjZiIsInN1YiI6IjY1MGYxYWQ2MjZkYWMxMDEyZDVhOTRjYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.P2xiPnZCI13rJqk-Pe7KWfIgVRbYf80777VvzGoNdik"}
 
     response = requests.get(url, headers=headers)
@@ -49,20 +50,18 @@ def home():
         return f"Erro status code: {response.status_code}"
     
 
-TMDB_API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMmM2YWNkNDM4OTVjZGVlMzkwNWRhODhjZDdjOTNjZiIsInN1YiI6IjY1MGYxYWQ2MjZkYWMxMDEyZDVhOTRjYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.P2xiPnZCI13rJqk-Pe7KWfIgVRbYf80777VvzGoNdik'
 
-@app.route('/search', methods=['GET', 'POST'])
+@app.route('/search',methods=['GET','POST'])
 def search():
     if request.method == 'POST':
         pesquisa_filme = request.form.get('query')
-        url = f"https://api.themoviedb.org/3/search/movie?query={pesquisa_filme}&include_adult=true&language=pt-BR&page=1"
-        
+        url = f"https://api.themoviedb.org/3/search/movie?query={pesquisa_filme}&include_adult=true&language=pt-BR&page=1"  
         headers = {
             "accept": "application/json",
             "Authorization": f"Bearer {TMDB_API_KEY}"
         }
-
         response = requests.get(url, headers=headers)
+
 
         if response.status_code == 200:
             dados = response.json()
@@ -70,13 +69,12 @@ def search():
         else:
             return f"Erro na pesquisa. Status code: {response.status_code}"
 
-    return render_template('search.html', dados=None)
+    return render_template('search.html', dados=None,pesquisa_filme=None)
 
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
-
 
 
 if __name__ == "__main__":
